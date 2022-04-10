c31ba89af0d0cfb1165a8c9f9cd05697e1bb2cf8
4shu10sh <ashutosh.dubey@appinventiv.com>
2022-04-10 16:41:44 +0530

    feed ui

---
+++ components/Details.js
@@ -0,0 +1,92 @@
+export const Details = [
+    {
+        dp: require('/Users/admin/Desktop/ReactNative/Instagram/assets/DP/1.webp'),
+        username: 'dattebayo',
+        likes: '1,345,334',
+        caption: 'Saaaaaasukkkkkeeeeeee 😡😡😡!!!!',
+        post: require('/Users/admin/Desktop/ReactNative/Instagram/assets/posts/1.webp'),
+        comments: '25,316',
+        time: '2 hours ago'
+    },
+    {
+        dp: require('/Users/admin/Desktop/ReactNative/Instagram/assets/DP/2.jpeg'),
+        username: 'godhimself',
+        likes: '32,453,543',
+        caption: 'I have met God -- He is me. 😎💪🔥',
+        post: require('/Users/admin/Desktop/ReactNative/Instagram/assets/posts/2.webp'),
+        comments: '65,417',
+        time: '2 hours ago'
+    },
+    {
+        dp: require('/Users/admin/Desktop/ReactNative/Instagram/assets/DP/3.png'),
+        username: 'detectiveofthecentury',
+        likes: '13,342,432',
+        caption: 'Sherlock who? 🤔',
+        post: require('/Users/admin/Desktop/ReactNative/Instagram/assets/posts/3.webp'),
+        comments: '95,319',
+        time: '3 hours ago'
+    },
+    {
+        dp: require('/Users/admin/Desktop/ReactNative/Instagram/assets/DP/4.jpeg'),
+        username: 'pirateking',
+        likes: '34,923,232',
+        caption: 'Where\'s my treasure? 🤩💰',
+        post: require('/Users/admin/Desktop/ReactNative/Instagram/assets/posts/4.webp'),
+        comments: '15,210',
+        time: '3 hours ago'
+    },
+    {
+        dp: require('/Users/admin/Desktop/ReactNative/Instagram/assets/DP/5.jpeg'),
+        username: 'moonchild',
+        likes: '428,342,987',
+        caption: 'I looked so cute as a kid 😊😊😊',
+        post: require('/Users/admin/Desktop/ReactNative/Instagram/assets/posts/5.webp'),
+        comments: '10,916',
+        time: '4 hours ago'
+    },
+    {
+        dp: require('/Users/admin/Desktop/ReactNative/Instagram/assets/DP/6.jpeg'),
+        username: 'kiki',
+        likes: '12,986,353',
+        caption: 'Contemplating about life...😌🌻',
+        post: require('/Users/admin/Desktop/ReactNative/Instagram/assets/posts/6.jpeg'),
+        comments: '65,656',
+        time: '4 hours ago'
+    },
+    {
+        dp: require('/Users/admin/Desktop/ReactNative/Instagram/assets/DP/7.webp'),
+        username: 'saitama',
+        likes: '999,999,999,999,999',
+        caption: 'When\'s the next sale coming? 😬',
+        post: require('/Users/admin/Desktop/ReactNative/Instagram/assets/posts/7.webp'),
+        comments: '88,316',
+        time: '5 hours ago'
+    },
+    {
+        dp: require('/Users/admin/Desktop/ReactNative/Instagram/assets/DP/8.jpeg'),
+        username: 'dogoodbegood',
+        likes: '98,768,004',
+        caption: 'I met Emi today heuheuheu ❤️❤️❤️',
+        post: require('/Users/admin/Desktop/ReactNative/Instagram/assets/posts/8.webp'),
+        comments: '25,316',
+        time: '5 hours ago'
+    },
+    {
+        dp: require('/Users/admin/Desktop/ReactNative/Instagram/assets/DP/9.webp'),
+        username: 'spiritedaway',
+        likes: '723,644,398',
+        caption: 'Not all spirits 💀 are scary! 😁',
+        post: require('/Users/admin/Desktop/ReactNative/Instagram/assets/posts/9.jpeg'),
+        comments: '98,123',
+        time: '6 hours ago'
+    },
+    {
+        dp: require('/Users/admin/Desktop/ReactNative/Instagram/assets/DP/10.jpeg'),
+        username: 'jiro',
+        likes: '34,786,321',
+        caption: 'Good day today.✨',
+        post: require('/Users/admin/Desktop/ReactNative/Instagram/assets/posts/10.jpeg'),
+        comments: '11,311',
+        time: '6 hours ago'
+    },
+]
--- components/Feed.js
+++ components/Feed.js
@@ -1,17 +1,177 @@
-import { View, Text, StyleSheet, Button } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+  FlatList,
+  Text,
+  TextInput,
+} from 'react-native';
+import React, { useState } from 'react';
+import {Details} from './Details';
 
 export default function Feed() {
+
+  const [comment, setComment] = useState('')
+
+  const onChangeText = (text) => {
+    setComment(text)
+  }
+
+  const onPress = () => {
+    setComment('')
+  }
+
+  const renderItem = ({item}) => {
+    return (
+      <View>
+        <View style={styles.header}>
+          <Image
+            source={item.dp}
+            style={styles.dp}
+          />
+          <Text
+            style={styles.text}>
+            {item.username}
+          </Text>
+          <Image
+            source={require('/Users/admin/Desktop/ReactNative/Instagram/assets/photos/option.png')}
+            style={{
+              height: 14,
+              width: 14,
+              marginRight: 10,
+              position: 'absolute',
+              alignSelf: 'center',
+              right: 0,
+              marginBottom: 10,
+            }}
+          />
+        </View>
+        <Image
+          source={item.post}
+          style={{width: 400, height: 400, marginBottom: 10}}
+        />
+        <View style={styles.header}>
+          <Image
+            source={require('/Users/admin/Desktop/ReactNative/Instagram/assets/photos/heart-empty.png')}
+            style={{height: 28, width: 28, marginLeft: 16, marginRight: 16}}
+          />
+          <Image
+            source={require('/Users/admin/Desktop/ReactNative/Instagram/assets/photos/chat.png')}
+            style={{height: 34, width: 34, marginRight: 12}}
+          />
+          <Image
+            source={require('/Users/admin/Desktop/ReactNative/Instagram/assets/photos/send.png')}
+            style={{height: 32, width: 32, marginRight: 210}}
+          />
+          <Image
+            source={require('/Users/admin/Desktop/ReactNative/Instagram/assets/photos/bookmark.png')}
+            style={{height: 24, width: 24}}
+          />
+        </View>
+        <Text style={[styles.text, {marginLeft: 10, marginTop: 6}]}>
+            {item.likes + ' likes'}
+            <Text style={{color:'#909090'}}>
+            {" • "+item.time}
+          </Text>
+        </Text>
+        <Text style={[styles.text, {marginLeft: 10}]}>
+            {item.username}
+            <Text style = {{fontWeight: '500'}}>
+            {' ' + item.caption}
+        </Text>
+        
+        </Text>
+        <Text style={{color: '#909090', marginLeft: 10, marginBottom: 10}}>
+          {`View all ${item.comments} comments`}
+        </Text>
+        <View style={styles.header}>
+          <Image
+            source={require('/Users/admin/Desktop/ReactNative/Instagram/assets/photos/user.jpg')}
+            style={styles.dp}
+          />
+          <TextInput
+          value={comment}
+          placeholder='Add a comment...'
+          placeholderTextColor={'#909090'}
+          style={{marginTop: -10, color: 'white', marginRight: 150}}
+          onChangeText={onChangeText}
+          />
+          {
+            comment.length>0 && 
+            <Text onPress={onPress} style={{color: '#3897f0', position: 'absolute', right: 30, bottom: 18}}>Post</Text>
+          }
+          </View>
+          
+      </View>
+    );
+  };
+  const ListFooterComponent = () => {
+    return <View></View>;
+  };
+
+  const ListHeaderComponent = () => {
+    return <View style={styles.listHeader} />;
+  };
+
   return (
-    <View style={styles.feed} >
-      <Text>Feed</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image
+          source={require('/Users/admin/Desktop/ReactNative/Instagram/assets/photos/ig.png')}
+          style={{height: 40, width: 140, marginRight: 160}}
+        />
+        <Image
+          source={require('/Users/admin/Desktop/ReactNative/Instagram/assets/photos/add.png')}
+          style={{height: 25, width: 25, marginRight: 30}}
+        />
+        <Image
+          source={require('/Users/admin/Desktop/ReactNative/Instagram/assets/photos/messenger.png')}
+          style={{height: 25, width: 25, marginRight: 10}}
+        />
+      </View>
+      <View style={styles.header}></View>
+      <FlatList
+        data={Details}
+        renderItem={renderItem}
+        showsVerticalScrollIndicator={false}
+        ListFooterComponent={ListFooterComponent}
+        ListHeaderComponent={ListHeaderComponent}
+        style={styles.list}
+      />
     </View>
-  )
+  );
 }
 const styles = StyleSheet.create({
-    feed: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    paddingTop: 30,
+  },
+  feed: {},
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 6,
+  },
+  listHeader: {
+    backgroundColor: '#282828',
+    height: 0.2,
+    width: 500,
+    marginBottom: 14,
+  },
+  text: {
+    color: 'white',
+    fontWeight: '700',
+    marginBottom: 10,
+  },
+  dp: {
+    height: 30,
+    width: 30,
+    borderRadius: 50,
+    marginRight: 10,
+    marginLeft: 10,
+    marginBottom: 10,
+    alignSelf: 'center',
+  }
+});
